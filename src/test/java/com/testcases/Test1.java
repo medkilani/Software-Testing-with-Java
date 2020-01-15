d1b264ec1440dc359ded69079582b269a51cb0eb
mkkilani <mohammed@mswdc.com>
2020-01-14 20:12:18 -0500

    First commit......

---
+++ src/test/java/com/testcases/Test1.java
@@ -0,0 +1,17 @@
+package com.testcases;
+
+public class Test1 {
+	public static void main(String[] args) {
+		
+		String hungry = "yes";
+		
+		if (hungry == "yes")
+			System.out.println("eat Something.......");
+		else {
+			
+			System.out.println("Do work.........");
+		}
+		
+	}
+
+}
