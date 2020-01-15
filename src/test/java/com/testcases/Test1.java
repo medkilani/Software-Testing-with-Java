58a7ad9cadd261dae1eaf2b4d34c965a2c904073
fakha <fakha@10.1.10.144>
2020-01-14 22:22:06 -0500

    committin test1 to branch Hyfan

--- src/test/java/com/testcases/Test1.java
+++ src/test/java/com/testcases/Test1.java
@@ -1,6 +1,15 @@
 package com.testcases;
 
 public class Test1 {
+	
+
+	public int add (int n1,int n2) {
+		
+		return n1+n2;
+		
+	}
+	
+	
 	public static void main(String[] args) {
 		
 		String hungry = "yes";
@@ -11,7 +20,13 @@ public class Test1 {
 			
 			System.out.println("Do work.........");
 		}
-		
+		Test1 obj = new Test1();
+		int haa =obj.add(45, 21);
+		System.out.println(haa);
 	}
+	}
+		
+		
+	
 
-}
+
